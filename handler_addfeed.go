336a6d02d7676b5ec9cc134f45293c1213549364
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-11 03:32:34 -0800

    Add feeds table with fields and implement addfeed command
    - Created `feeds` table with `id`, `created_at`, `updated_at`, `name`, `url`, and `user_id` fields
    - Ensured `url` is unique for preventing downloading duplicate posts
    - Applied `ON DELETE CASCADE` for user_id to manage orphaned records
    - Added query and command to handle feed addition

---
+++ handler_addfeed.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/katsuikeda/gator/internal/database"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("usage: %s <feed_name> <url>", cmd.name)
+	}
+
+	currentUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	feedName := cmd.args[0]
+	feedURL := cmd.args[1]
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      feedName,
+		Url:       feedURL,
+		UserID:    currentUser.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed %w", err)
+	}
+
+	fmt.Println("Feed created successfully:")
+	printFeed(feed)
+	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf(" * ID:      %v\n", feed.ID)
+	fmt.Printf(" * Name:    %v\n", feed.Name)
+}
