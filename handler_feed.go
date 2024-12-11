da04894bea6a168faa59dac410c6114094b7bdbc
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-11 04:09:26 -0800

    Add feeds command and the corresponding handler - feeds command lists all RSS feeds with the username that added the feed to db - Add GetFeeds query to read feeds from db Add GetUserById query to read user by id

--- handler_feed.go
+++ handler_feed.go
@@ -40,6 +40,36 @@ func handlerAddFeed(s *state, cmd command) error {
 }
 
 func printFeed(feed database.Feed) {
-	fmt.Printf(" * ID:      %v\n", feed.ID)
-	fmt.Printf(" * Name:    %v\n", feed.Name)
+	fmt.Printf("* ID:      %v\n", feed.ID)
+	fmt.Printf("* Name:    %v\n", feed.Name)
+	fmt.Printf("* URL:     %v\n", feed.Url)
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("usage: %s", cmd.name)
+	}
+
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feeds: %w", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for _, feed := range feeds {
+		user, err := s.db.GetUserById(context.Background(), feed.UserID)
+		if err != nil {
+			return fmt.Errorf("couldn't find user with the id")
+		}
+		fmt.Printf("* %s\n", feed.Name)
+		fmt.Printf("  - URL:      %s\n", feed.Url)
+		fmt.Printf("  - Added by: %s\n", user.Name)
+	}
+
+	return nil
 }
