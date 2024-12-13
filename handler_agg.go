d3ce7fd07059d78b598fb0a766b7e47143b84a34
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-12 16:59:45 -0800

    Implement continuous feed aggregation
    - Added `last_fetched_at` column to `feeds` table with new migration.
    - Implemented `MarkFeedFetched` query to update fetch timestamps.
    - Created `GetNextFeedToFetch` query to determine the next feed to fetch based on least recently fetched.
    - Wrote `scrapeNextFeedFromQueue` function to fetch and print post titles from feeds.
    - Wrote `scrapeFeed` function to mark and fetch feed
    - Updated `agg` command to accept `time_between_reqs` argument and run `scrapeNextFeedFromQueue` in a loop using `time.Ticker`.

--- handler_agg.go
+++ handler_agg.go
@@ -3,20 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/katsuikeda/gator/internal/database"
 	"github.com/katsuikeda/gator/internal/rss"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
-		return fmt.Errorf("usage: %s", cmd.name)
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.name)
 	}
 
-	feed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("couldn't fetch RSS feed: %w", err)
+		return fmt.Errorf("invalid duration: %w", err)
 	}
 
-	fmt.Printf("Fetched RSS feed successfully:\n %+v\n", feed)
-	return nil
+	log.Printf("Collecting feeds every %s...", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+	for ; ; <-ticker.C {
+		scrapeNextFeedFromQueue(s)
+	}
+}
+
+func scrapeNextFeedFromQueue(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Println("Couldn't get next feed to fetch:", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := rss.FetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+	}
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+
+	log.Printf("Feed %s collected, %d posts found", feed.Name, len(feedData.Channel.Item))
 }
